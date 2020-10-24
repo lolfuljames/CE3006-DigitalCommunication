7b556b4e9473e84840402054b208959d492bd01e
Poon6297 <43109885+Poon6297@users.noreply.github.com>
2020-10-24 18:39:02 +0800

    Updated generate noise function

--- generate_noise.m
+++ generate_noise.m
@@ -1,6 +1,7 @@
-function [generated_noise] = generate_noise(data_length, noise_power)
+function [generated_noise] = generate_noise(data_length, signal_power, snr_i)
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
-generated_noise = sqrt(noise_power/2) .* randn(1, data_length);
+noise_power = signal_power ./ snr_i;
+generated_noise = sqrt(noise_power / 2) .* randn(1, data_length);
 end
 
