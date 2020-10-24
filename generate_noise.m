e4832716a1bf3cada3811105694527bad8b0f230
Poon6297 <43109885+Poon6297@users.noreply.github.com>
2020-10-24 18:26:12 +0800

    Added generate noise function

---
+++ generate_noise.m
@@ -0,0 +1,6 @@
+function [generated_noise] = generate_noise(data_length)
+%UNTITLED4 Summary of this function goes here
+%   Detailed explanation goes here
+generated_noise = randn(1, data_length);
+end
+
