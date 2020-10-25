40316bf32ba74d86f29520b16b548a4a717a493b
Poon6297 <43109885+Poon6297@users.noreply.github.com>
2020-10-25 14:33:14 +0800

    Corrected noise variance addition

--- generate_noise.m
+++ generate_noise.m
@@ -1,6 +1,6 @@
 function [generated_noise] = generate_noise(data_length, noise_power)
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
-generated_noise = sqrt(noise_power / 2) .* randn(1, data_length);
+generated_noise = sqrt(noise_power) .* randn(1, data_length);
 end
 
