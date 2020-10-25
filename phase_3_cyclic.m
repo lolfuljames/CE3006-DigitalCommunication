0daf80c176fded2215417999e83722d119200d35
thatalfredh <bizcelearningresources@gmail.com>
2020-10-25 16:59:19 +0800

    Debug done. Need to add plots.

--- phase_3_cyclic.m
+++ phase_3_cyclic.m
@@ -3,17 +3,21 @@ clear all; close all; clc;
 % Codeword length (n) & Message length (k)
 codeword_length = 7;
 message_length = 4;
+
 % Data length (1024) & Encoded data length (1024/4 * 7 = 1792)
 signal_length = 1024;
 encoded_signal_length = signal_length/message_length * codeword_length;
+
 % Carrier parameters and signal (amplitude = 5) as required
 carrier_freq = 10000;
 sampling_freq = 16 * carrier_freq;
 data_rate = 1000;
 t = 0: 1/sampling_freq : encoded_signal_length/data_rate;
 carrier_signal = 5 .* cos(2*pi*carrier_freq*t);
+
 % Sampled signal length
 sampled_signal_length = sampling_freq*encoded_signal_length/data_rate + 1;
+
 % Number of samples
 nb_samples = 20;
 % Low-pass filter & High-pass filter - 6th order, 0.2 cutoff frequency
@@ -53,7 +57,7 @@ for i = 1 : length(SNR)
         
         % Sampling
         sampled_signal = zeros(1, sampled_signal_length);
-        for k = 1: signal_length - 1
+        for k = 1: sampled_signal_length - 1
             sampled_signal(k) = encoded_signal(ceil(k*data_rate/sampling_freq));
         end
         sampled_signal(sampled_signal_length) = sampled_signal(sampled_signal_length - 1);
@@ -70,10 +74,10 @@ for i = 1 : length(SNR)
         
         % Generate White Gaussian Channel Noise for OOK and BPSK
         OOK_noise_power = OOK_signal_power ./ SNR(i);
-        OOK_noise = sqrt(OOK_noise_power/2) .*randn(1,signal_length);
+        OOK_noise = sqrt(OOK_noise_power/2) .*randn(1,sampled_signal_length);
         
         BPSK_noise_power = BPSK_signal_power ./ SNR(i);
-        BPSK_noise = sqrt(BPSK_noise_power/2) .* randn(1, signal_length);
+        BPSK_noise = sqrt(BPSK_noise_power/2) .* randn(1,sampled_signal_length);
         
         % Received Signal with added Channel Noise for OOK and BPSK
         OOK_received = OOK_signal + OOK_noise;
@@ -100,37 +104,21 @@ for i = 1 : length(SNR)
         
         % Demodulation: Sampling and Threshold
         sampling_period = sampling_freq/data_rate;
-        [OOK_sample, OOK_result] = sample_and_threshold(OOK_filtered, sampling_period, amp/2, codeword_length);
-        [BPSK_sample, BPSK_result] = sample_and_threshold(BPSK_output, sampling_period, 0, encoded_length);
+        [OOK_sample, OOK_result] = sample_and_threshold(OOK_filtered, sampling_period, 5/2, encoded_signal_length);
+        [BPSK_sample, BPSK_result] = sample_and_threshold(BPSK_output, sampling_period, 0, encoded_signal_length);
         
         % Cyclic Decoding
         decoded_signal_OOK = decode(OOK_result, codeword_length, message_length, 'cyclic/binary', genpoly, trt);
         decoded_signal_BPSK = decode(BPSK_result, codeword_length, message_length, 'cyclic/binary', genpoly, trt);
         
         % Get Bit Errors
-        OOK_error_ber = biterr(decoded_signal_OOK, orignal_signal);
-        BPSK_error_ber = biterr(decoded_signal_BPSK, original_signal);
+        OOK_error = biterr(decoded_signal_OOK, original_signal);
+        BPSK_error = biterr(decoded_signal_BPSK, original_signal);
         
-        OOK_error = 0;
-        BPSK_error = 0;
-        for k = 1: signal_length - 1
-        	if(decoded_signal_OOK(k) ~= original_signal(k))
-               OOK_error = OOK_error + 1;
-         end
-         if(decoded_signal_BPSK(k) ~= original_signal(k))
-               BPSK_error = BPSK_error + 1;
-         end
-        end
-		
-        disp(OOK_error_ber)
-        disp(BPSK_error_ber)
-        disp(OOK_error)
-        disp(BPSK_error)
-		
         OOK_average_error = OOK_error + OOK_average_error;
         BPSK_average_error = BPSK_error + BPSK_average_error;
     end
     
-    OOK_error_rate(i) = OOK_average_error / test_samples;
-    BPSK_error_rate(i) = BPSK_average_error / test_samples;
+    OOK_error_rate(i) = OOK_average_error / nb_samples;
+    BPSK_error_rate(i) = BPSK_average_error / nb_samples;
 end
